742f91cb465f661879177da25f43787a5fe418d1
Anna Wójcik <aniawojcik894@gmail.com>
2025-07-23 18:01:34 +0200

    Margin of Main styled component changed

--- src/common/Main/styled.js
+++ src/common/Main/styled.js
@@ -5,8 +5,14 @@ export const Main = styled.main`
     max-width: 1408px;
     width: 100%;
     padding: 0 20px;
+    margin-bottom: 80px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+        margin-bottom: 60px;
+    }
 
     @media(max-width: ${({ theme }) => theme.breakpoint.smallMax}px) {
         padding: 0 16px;
+        margin-bottom: 40px;
     }
 `;
